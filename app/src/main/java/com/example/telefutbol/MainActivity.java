b6787adef1c25fd7904577681a2b2789e8115061
Diego Henriquez <a20182044@pucp.edu.pe>
2024-10-04 12:01:53 -0500

    Add:Pantalla principal de la app-PREG1

--- app/src/main/java/com/example/telefutbol/MainActivity.java
+++ app/src/main/java/com/example/telefutbol/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.telefutbol;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -27,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         /******************************* Internet *****************************/
-        Toast.makeText(this, "Tiene internet: " + tengoInternet(), Toast.LENGTH_LONG).show();
+        if(tengoInternet()){
+            Toast.makeText(this, "Hay conexion a internet " , Toast.LENGTH_LONG).show();
+        }else {
+            mostrarNoInternetDialog();
+        }
 
     }
 
@@ -54,5 +62,22 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return tieneInternet;
+    }
+
+    private void mostrarNoInternetDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Sin conexión a Internet")
+                .setMessage("No tienes conexión a Internet. ¿Quieres ir a la configuración?")
+                .setCancelable(false) // No permitir cerrar el diálogo sin acción
+                .setPositiveButton("Configuración", (dialog, which) -> {
+                    // Redirigir a la configuración del dispositivo
+                    Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                    startActivity(intent);
+                })
+                .setNegativeButton("Salir", (dialog, which) -> {
+                    // Cerrar la aplicación
+                    finish();
+                })
+                .show();
     }
 }
